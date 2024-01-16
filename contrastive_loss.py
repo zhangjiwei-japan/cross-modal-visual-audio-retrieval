0f27f38c323249edc6d83453dd03e801a1b50e3f
Jiwei Zhang <58073128+zhangjiwei-japan@users.noreply.github.com>
2024-01-16 11:34:11 +0900

    Update contrastive_loss.py

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -3,62 +3,6 @@ import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ContrastiveLoss(nn.Module):
-    def __init__(self, batch_size, device='cuda', temperature=0.5):
-        super().__init__()
-        self.batch_size = batch_size
-        self.register_buffer("temperature", torch.tensor(temperature).to(device))			
-        self.register_buffer("negatives_mask", (~torch.eye(batch_size * 2, batch_size * 2, dtype=bool).to(device)).float())		
-        
-    def forward(self, emb_i, emb_j):		# emb_i, emb_j 
-        z_i = F.normalize(emb_i, dim=1)     # (bs, dim)  --->  (bs, dim)
-        z_j = F.normalize(emb_j, dim=1)     # (bs, dim)  --->  (bs, dim)
-
-        representations = torch.cat([z_i, z_j], dim=0)          # repre: (2*bs, dim)
-        similarity_matrix = F.cosine_similarity(representations.unsqueeze(1), representations.unsqueeze(0), dim=2)      # simi_mat: (2*bs, 2*bs)
-        
-        sim_ij = torch.diag(similarity_matrix, self.batch_size)         # bs
-        sim_ji = torch.diag(similarity_matrix, -self.batch_size)        # bs
-        positives = torch.cat([sim_ij, sim_ji], dim=0)                  # 2*bs
-        
-        nominator = torch.exp(positives / self.temperature)             # 2*bs
-        denominator = self.negatives_mask * torch.exp(similarity_matrix / self.temperature)             # 2*bs, 2*bs
-    
-        loss_partial = -torch.log(nominator / torch.sum(denominator, dim=1))        # 2*bs
-        loss = torch.sum(loss_partial) / (2 * self.batch_size)
-        return loss
-
-class SimilarityLoss(nn.Module):
-    def __init__(self, batch_size, device='cuda', hyper_lambda=0.5):
-        super().__init__()
-        self.batch_size = batch_size
-        self.register_buffer("hyper_lambda", torch.tensor(hyper_lambda).to(device))			
-        self.register_buffer("negatives_mask", (~torch.eye(batch_size * 2, batch_size * 2, dtype=bool).to(device)).float())		
-        
-    def forward(self, emb_i, emb_j):		# emb_i, emb_j 
-        z_i = F.normalize(emb_i, dim=1)     # (bs, dim)  --->  (bs, dim)
-        z_j = F.normalize(emb_j, dim=1)     # (bs, dim)  --->  (bs, dim)
-
-        representations = torch.cat([z_i, z_j], dim=0)          # repre: (2*bs, dim)
-        similarity_matrix = F.cosine_similarity(representations.unsqueeze(1), representations.unsqueeze(0), dim=2)      # simi_mat: (2*bs, 2*bs)
-        
-        sim_ij = torch.diag(similarity_matrix, self.batch_size)         # bs
-        sim_ji = torch.diag(similarity_matrix, -self.batch_size)        # bs
-        positives = torch.cat([sim_ij, sim_ji], dim=0)                  # 2*bs
-        positives_exp = torch.exp(-positives + self.hyper_lambda)
-        negatives_exp = self.negatives_mask * torch.exp(similarity_matrix - self.hyper_lambda)
-        positives_similarity = torch.log(1 + positives_exp)
-        negatives_similarity = torch.log(1 + negatives_exp)
-        positives_sum = torch.sum(positives_similarity) / (2 * self.batch_size)
-        negatives_sum = torch.sum(negatives_similarity) / (12 * self.batch_size)
-        # nominator = torch.exp(positives - self.hyper_lambda)             # 2*bs
-        # denominator = self.negatives_mask * torch.exp(similarity_matrix / self.hyper_lambda)             # 2*bs, 2*bs
-    
-        # loss_partial = -torch.log(nominator / torch.sum(denominator, dim=1))        # 2*bs
-        # loss = torch.sum(loss_partial) / (2 * self.batch_size)
-        loss = positives_sum + negatives_sum 
-        return loss
-    
 class IntraModalLoss(nn.Module):
     def __init__(self, batch_size, device='cuda', temperature=0.2, hyper_gamma=0.5):
         super().__init__()
